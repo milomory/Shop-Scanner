770a05dbf1299b3cf1e0678f12f9e00ca5948802
mil <milomory@gmail.com>
2024-08-12 11:44:07 +0300

    init app

---
+++ app/config/env.config.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+interface EnvConfig {
+    DB_HOST: string | undefined;
+    DB_USER: string | undefined;
+    DB_PASSWORD: string | undefined;
+    DB_NAME: string | undefined;
+    DB_DIALECT: string | undefined;
+    DB_PORT: string | undefined;
+}
+
+export const getEnv = () => {
+    return {
+        DB_HOST: process.env.DB_HOST,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_NAME: process.env.DB_NAME,
+        DB_DIALECT: process.env.DB_DIALECT,
+        DB_PORT: process.env.DB_PORT
+    };
+};
